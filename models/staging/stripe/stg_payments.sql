29535e316c8dd932ea85ba5b42fa1c5ea6e780e0
alberto.perez@lifullconnect.com <alberto.perez@lifullconnect.com>
2021-11-04 09:25:58 +0000

    Sources. Add source to stripe ddbb

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,4 +1,17 @@
-with payments as (
+-- with payments as (
+--     select 
+--         id as payment_id,
+--         orderid as order_id,
+--         paymentmethod as payment_method,
+--         status,
+--         -- amount is stored in cents, convert it to dollars
+--         amount / 100 as amount, 
+--         created as created_at
+
+--     from raw.stripe.payment
+-- )
+
+select * from payments
     select 
         id as payment_id,
         orderid as order_id,
@@ -8,7 +21,4 @@ with payments as (
         amount / 100 as amount, 
         created as created_at
 
-    from raw.stripe.payment
-)
-
-select * from payments
+from {{ source('stripe', 'payment')}}
