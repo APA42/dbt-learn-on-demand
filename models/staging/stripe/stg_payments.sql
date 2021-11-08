7feb2bba34e486cfe4398c10bdd753abf73c53da
alberto.perez@lifullconnect.com <alberto.perez@lifullconnect.com>
2021-11-08 10:33:32 +0000

    Macros. First example

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -17,7 +17,7 @@ select
     paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
-    amount / 100 as amount, 
+    {{ cents_to_dollars() }} as amount, 
     created as created_at
 
 from {{ source('stripe', 'payment')}}
